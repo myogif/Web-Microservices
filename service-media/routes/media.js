2e24ab4ad8ea0d3d8c675843224192943d6db062
myogic <moh.yogi10065@gmail.com>
2022-01-28 14:57:01 +0700

    add method DELETE

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
-
+const fs = require('fs');
 const {Media} = require('../models')
+
+
 
 router.get('/', async (req, res) => {
   const media = await Media.findAll({
@@ -50,7 +52,27 @@ router.post('/', (req, res)=>{
     })
   });
 
-})
+});
+
+router.delete('/:id', async (req, res)=>{
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+  if(!media){
+    return res.status(404).json({status: 'error', message: 'media not found !'});
+  }
+  fs.unlink(`./public/${media.image}`, async (err) =>{
+    if(err){
+      return res.status(400).json({status: "error", message: err.message});
+    }
+    await media.destroy();
+
+    return res.json({
+      status: 'success',
+      message: 'image deleted'
+    });
+  });
+});
 
 
 
