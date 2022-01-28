be4a31abcb03e9b5a98134017b780f774b1cc058
myogic <moh.yogi10065@gmail.com>
2022-01-28 14:23:48 +0700

    ad POST/GET

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -1,9 +1,57 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const isBase64 = require('is-base64');
+const base64Img = require('base64-img');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const {Media} = require('../models')
+
+router.get('/', async (req, res) => {
+  const media = await Media.findAll({
+    attributes: ['id', 'image']
+  });
+
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m; 
+  })
+
+
+  return res.json({
+    status: 'success',
+    data: mappedMedia
+  });
 });
+
+
+router.post('/', (req, res)=>{
+  const image = req.body.image;
+
+  if(!isBase64(image,{mimeRequired: true})){
+    return res.status(400).json({status: 'error', message: 'Invalid base64'})
+  }
+
+  base64Img.img(image, './public/images', Date.now(),async (err, filepath) =>{
+    if(err){
+      return res.status(400).json({status: 'errorr', message: err.message});
+    }
+
+
+    // '/public/images/20220129.png' 
+    // just take a filename png
+    const filename = filepath.split('/').pop() // take a last array
+    const media = await Media.create({image: `images/${filename}`});
+    
+    return res.json({
+      status: 'success',
+      data:{
+        id: media.id,
+        image: `${req.get('host')}/images/${filename}`
+      }
+    })
+  });
+
+})
+
+
 
 module.exports = router;
